769eb952b194401d2d7e3629e2120268d8a671a2
Derek <derek@meer.email>
2024-08-29 02:04:07 -0700

    add in some more functionality + TODOs for everything else

--- lib/ultraviolet/scale.ex
+++ lib/ultraviolet/scale.ex
@@ -25,25 +25,122 @@ defmodule Ultraviolet.Scale do
   """
   def new(), do: {:ok, struct(Scale)}
   def new(colors, options \\ []) when is_list(colors) and is_list(options) do
-    case Enum.find(colors, &!match?(%{__struct__: Color}, &1)) do
-      nil ->
-        {:ok, struct(Scale, Keyword.put(options, :colors, colors))}
-      other ->
-        {:error, "#{inspect(other)} is not a Color"}
+    with nil <- Enum.find(colors, &!match?(%{__struct__: Color}, &1)),
+         {:ok, opts} <- validate_scale_options(options, colors) do
+      {:ok, struct(Scale, Map.put(opts, :colors, colors))}
+    else
+      {:error, _} = error -> error
+      other -> {:error, "#{inspect(other)} is not a Color"}
     end
   end
 
   @doc """
-  Retrieves a single color from the scale at the given value in the domain.
+  merges the `options` with given `Scale` and returns the new `Scale`.
   """
-  def fetch(_scale, _x), do: Color.new("black")
+  def with_options(%Scale{} = scale, options) when is_list(options) do
+    case validate_scale_options(options, scale.colors) do
+      {:ok, opts} -> Map.merge(scale, opts)
+      _ -> scale
+    end
+  end
+
+  defp validate_scale_options(options, _colors) when is_list(options) do
+    options
+    |> Enum.into(%{})
+    |> Map.take([:space, :domain, :padding, :gamma, :correct_lightness?, :classes, :interpolation])
+    # TODO add more validations here
+    |> ok_if_no_error()
+  end
+
+  defp ok_if_no_error({:error, _} = error), do: error
+  defp ok_if_no_error(options), do: {:ok, options}
+
+  @doc """
+  Retrieves a single color from the `scale` at the given `value `in the domain.
+  """
+  def fetch(scale, value, bypass? \\ false)
+
+  def fetch(%Scale{} = scale, value, bypass?) when is_number(value) do
+    with {:ok, x} <- domain(scale, value, bypass?) do
+      interpolate(scale, x)
+    else
+      error -> error
+    end
+  end
+
+  def fetch(%Scale{}, _, _), do: {:error, :domain}
+
+  defp domain(%Scale{} = scale, value, true) do
+    apply_functions_or_error(
+      scale,
+      # TODO write these functions
+      [&domain_map/2, &gamma/2, &pad/2, &limit/2],
+      value,
+    )
+  end
+
+  defp domain(%Scale{} = scale, value, _no_bypass) do
+    apply_functions_or_error(
+      scale,
+      [&classify/2, &domain_map/2, &lightness/2, &gamma/2, &pad/2, &limit/2],
+      value,
+    )
+  end
+
+  # TODO figure out classification
+  defp classify(_scale, x), do: {:ok, x}
+
+  # TODO maybe support domain/color mismatch
+  defp domain_map(_scale, x), do: {:ok, x}
+
+  defp lightness(%{correct_lightness?: false}, x), do: {:ok, x}
+  # TODO figure this out
+  defp lightness(_scale, x) do
+    {:ok, x}
+  end
+
+  defp gamma(%{gamma: 1}, x), do: {:ok, x}
+  defp gamma(%{gamma: g}, x), do: {:ok, :math.pow(x, g)}
+
+  defp pad(%{padding: 0}, x), do: {:ok, x}
+  defp pad(%{padding: [0, 0]}, x), do: {:ok, x}
+  defp pad(%{padding: [l, r]}, x), do: {:ok, l + x * (1 - l - r)}
+
+  defp pad(%{padding: padding}, x) when is_number(padding) do
+    pad(%{padding: [padding, padding]}, x)
+  end
+
+  defp limit(_scale, x), do: {:ok, min(max(x, 0), 1)}
+
+  defp apply_functions_or_error(scale, fns, initial_value) do
+    Enum.reduce_while(
+      fns,
+      {:ok, initial_value},
+      fn f, {:ok, x} ->
+        case f.(scale, x) do
+          {:error, _} = error -> {:halt, error}
+          result -> {:cont, result}
+        end
+      end
+    )
+  end
+
+  defp interpolate(_scale, x) do
+    # TODO interpolate based on interpolation method (linear, custom, bezier, etc.)
+    Color.new("black")
+  end
 
   @doc """
   Retrieves a single color from the scale at the given value in the domain.
   If the given value is outside of the domain or otherwise invalid, returns the
   `default` color.
   """
-  def get(_scale, _x, default), do: default
+  def get(scale, x, default) do
+    case fetch(scale, x) do
+      {:ok, color} -> color
+      _ -> default
+    end
+  end
 
   @doc """
   ### If `xs` is a list:
@@ -56,6 +153,19 @@ defmodule Ultraviolet.Scale do
 
   Returns a list of `xs` equi-distant colors from the scale.
   """
-  def take(_scale, xs) when is_list(xs), do: %{}
-  def take(_scale, n) when is_integer(n) and n > 0, do: []
+  def take(scale, xs) when is_list(xs) do
+    xs
+    |> Enum.flat_map(xs, fn x ->
+      case get(scale, x, nil) do
+        nil -> []
+        color -> [{x, color}]
+    end)
+    |> Enum.into(%{})
+  end
+
+  def take(%{domain: d} = scale, n) when is_integer(n) and n > 0 do
+    max = hd(Enum.reverse(d))
+    min = hd(d)
+    take(scale, Enum.map(0..n-1, fn i -> i * (max - min) / (n - 1) end))
+  end
 end
